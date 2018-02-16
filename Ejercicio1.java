4b13ea68e16ec71b4b508d79fa5bbf7488ae3693
Unknown <camilomedina1508@gmail.com>
2018-02-16 14:16:07 -0500

    Mejora de version

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -2,12 +2,17 @@
 
 public class Ejercicio1 {
       public static void main(String[ ] arg) {
-        int Numero1= 10;
+        int Numero1=20;
         int Numero2=20;
-        if (Numero1>Numero2)
-        System.out.println(Numero1 + " es mayor que " + Numero2);
-        else 
-        System.out.println(Numero2 + " es mayor que " + Numero1);
+        if (Numero1>Numero2){
+            System.out.println(Numero1 + " es mayor que " + Numero2);
+        }
+        if (Numero1<Numero2){
+            System.out.println(Numero2 + " es mayor que " + Numero1);
+        }
+        if(Numero1==Numero2){
+        	System.out.println(Numero2 + " es igual a " + Numero1);
+        }
       }
 
 }
