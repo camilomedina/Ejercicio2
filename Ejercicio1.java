423d9e89b04373662b3b512edab2414cd2e98134
Unknown <camilomedina1508@gmail.com>
2018-02-16 14:03:42 -0500

    Diferencia o igualdad entre dos numeros

---
+++ Ejercicio1.java
@@ -0,0 +1,13 @@
+
+
+public class Ejercicio1 {
+      public static void main(String[ ] arg) {
+        int Numero1= 10;
+        int Numero2=20;
+        if (Numero1>Numero2)
+        System.out.println(Numero1 + " es mayor que " + Numero2);
+        else 
+        System.out.println(Numero2 + " es mayor que " + Numero1);
+      }
+
+}
